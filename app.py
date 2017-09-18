67be68bb6bb392b43954c3479eab06e910032148
PrabhuMMoorthy <mprabhumoorthy@gmail.com>
2017-09-18 06:56:49 +0000

    Updated port numbers

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def relayRequest(req):
     if req.get("result").get("action") == "request_name_permission":
         baseurl = "https://us-central1-namepsychicdemo-6bd8d.cloudfunctions.net/namePsychic/"
     else:
-        baseurl = "http://34.203.152.187:9001/highstreetcommercewebservices/v2/highstreet/webhook/"
+        baseurl = "http://34.203.152.187/highstreetcommercewebservices/v2/highstreet/webhook/"
     reqObj = urllib.request.Request(baseurl)
     reqObj.add_header('Content-Type', 'application/json; charset=utf-8')
     jsondata = json.dumps(req)
