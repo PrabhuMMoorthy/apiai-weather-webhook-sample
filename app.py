f36c6d3bb1eda27f0a21d816929d315c45f3db30
PrabhuMMoorthy <mprabhumoorthy@gmail.com>
2018-02-07 12:38:13 +0530

    Google home prod code

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from urllib.error import HTTPError
 import json
 import os
 import re
-import websocket
+import urllib.request
 
 from flask import Flask
 from flask import request
@@ -24,139 +24,91 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
-    print("Request: Latest 0001")
+    
+    print("Request: Latest 002")
     print(json.dumps(req, indent=4))
-
-    res = processRequest(req)
-
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-	
-def suggestDeodrant(condition, person, city):
-    print(person)
-    humidWeatherList = ['Cloudy','mostly cloudy (night)','mostly cloudy (day)','partly cloudy (night)','partlycloudy (day)','tornado','tropical storm','hurricane','severe thunderstorms','thunderstorms','mixed rain and snow','mixed rain and sleet','mixed snow and sleet','freezing drizzle','drizzle','freezing rain','Showers','snow flurries','light snow showers','blowing snow','snow','hail','sleet','mixed rain and hail','thundershowers','snow showers','isolated','thundershowers'];
-    hotWeatherList = ['dust','foggy','haze','smoky','blustery','windy','cold','clear (night)','sunny','fair (night)','fair (day)','hot','isolated thunderstorms','scattered thunderstorms','scattered thunderstorms','scattered showers','heavy snow','scattered snow showers','heavy snow','partly cloudy'];
-    if(condition in humidWeatherList):
-     print('humid')
-     men = 'Men'
-     if(person.lower()==men.lower()):
-      condition = 'Hmmm.. The weather in '+city+' looks '+condition+'. I suggest these <a target="_blank" href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Men-Perfumes/Moist/c/580">Anti-Perspirant Deodrants</a> for ' + person
-     else:
-      condition = 'Hmmm.. The weather in '+city+' looks '+condition+'. I suggest these <a target="_blank" href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Women-Perfumes/Moist/c/395">Anti-Perspirant Deodrants</a> for ' + person
+    res = relayRequest(req)
+    print(res)
+    channel = "device"
+    userIdHybris = ""
+    contextList = req.get("result").get("contexts")
+    for context in contextList:
+        print("Context: ")
+        if context.get("name") == "channel" :
+            channel = context.get("parameters").get("name")
+            userIdHybris = context.get("parameters").get("userIdHybris")
+            print(channel+" - "+userIdHybris)
+            print("==========================")
+    if req.get("result").get("action") == "getUserDetails":
+        res = json.dumps(res, indent=4)
+        #print(res)
+        r = make_response(res)
+        r.headers['Content-Type'] = 'application/json'
+        print("json formed in python")
+    elif (channel == "desktop" and req.get("result").get("action") == "request_name_permission"):
+        res = json.dumps(res, indent=4)
+        #print(res)
+        r = make_response(res)
+        r.headers['Content-Type'] = 'application/json'
+        print("json formed in python - desktop flow")
+        return r
     else:
-     print('dry')
-     menv = 'Men'
-     if(person.lower()==menv.lower()):
-      condition = 'Hmmm.. The weather in '+city+' looks '+condition+'. I suggest these <a target="_blank" href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Men-Perfumes/Dry/c/570">Perfumed Deodrants</a> for ' + person
-     else:
-      condition = 'Hmmm.. The weather in '+city+' looks '+condition+'. I suggest these <a target="_blank" href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Women-Perfumes/Dry/c/390">Perfumed Deodrants</a> for ' + person
-    return condition
+        print("dummy")
 
-def getCategoryLink(condition, person, city):
-    print(person)
-    humidWeatherList = ['Cloudy','mostly cloudy (night)','mostly cloudy (day)','partly cloudy (night)','partlycloudy (day)','tornado','tropical storm','hurricane','severe thunderstorms','thunderstorms','mixed rain and snow','mixed rain and sleet','mixed snow and sleet','freezing drizzle','drizzle','freezing rain','Showers','snow flurries','light snow showers','blowing snow','snow','hail','sleet','mixed rain and hail','thundershowers','snow showers','isolated','thundershowers'];
-    hotWeatherList = ['dust','foggy','haze','smoky','blustery','windy','cold','clear (night)','sunny','fair (night)','fair (day)','hot','isolated thunderstorms','scattered thunderstorms','scattered thunderstorms','scattered showers','heavy snow','scattered snow showers','heavy snow','partly cloudy'];
-    if(condition in humidWeatherList):
-     print('humid')
-     men = 'Men'
-     if(person.lower()==men.lower()):
-      condition = '<a target="_blank" href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Men-Perfumes/Moist/c/580">Anti-Perspirant Deodrants</a>'
-     else:
-      condition = '<a target="_blank" href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Women-Perfumes/Moist/c/395">Anti-Perspirant Deodrants</a>'
-    else:
-     print('dry')
-     menv = 'Men'
-     if(person.lower()==menv.lower()):
-      condition = '<a target="_blank" href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Men-Perfumes/Dry/c/570">Perfumed Deodrants</a>'
-     else:
-      condition = '<a target="_blank" href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Women-Perfumes/Dry/c/390">Perfumed Deodrants</a>'
-    return condition
-
-def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data,req)
     return res
 
+def relayRequest(req):
+    print("############### ACTION - "+req.get("result").get("action"))
+    channel = "device"
+    userIdHybris = ""
+    contextList = req.get("result").get("contexts")
+    for context in contextList:
+        print("Context :"+context.get("name"))	
+        if context.get("name") == "channel" :
+            print("matched...")
+            channel = context.get("parameters").get("name")
+            print(channel)
+            userIdHybris = context.get("parameters").get("userIdHybris")
+            print(userIdHybris)
+            print("---------------------------------------")
 
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
-
-def makeWebhookResult(data, req):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    #speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-    #         ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-	
-    speech = "Hmmm.. It looks " + condition.get('text') + " in " + location.get('city')
-    airesult = req.get("result")
-    parameters = airesult.get("parameters")
-    person = parameters.get('Person')
-    city = parameters.get("geo-city")
-    conditionText = condition.get('text')
-    returnedSpeech = suggestDeodrant(condition.get('text'), person, city)
-    categoryLink = getCategoryLink(condition.get('text'), person, city)
-    print(returnedSpeech)
-    #print("Response:")
-    #print(speech)
-    print("Starting SOCKET Connection#########################################################")
-    websocket.enableTrace(True)
-    ws = websocket.create_connection("wss://rocky-plateau-29722.herokuapp.com/socket")
-    print("Sending 'Hello, World'...")
-    ws.send('{"event": "refrestPageFromPython", "user": "Prabhu", "url":"/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Men-Perfumes/Latest/c/510"}')
-    print("Sent")
-    ws.close()
-
-    return {
-        "speech": returnedSpeech,
-        "displayText": returnedSpeech,
-	"followupEvent": {"name":"user_location_ip", "data":{"person":person,"categoryLink":categoryLink,"conditionText":conditionText,"travel_to":city,"travel_from":"$travel_from", "returnedSpeech":returnedSpeech}},
-        # "data": data.,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
-
+    if req.get("result").get("action") == "getUserDetails" :
+        displayName = req.get("originalRequest").get("data").get("user").get("profile").get("displayName")
+        givenName = req.get("originalRequest").get("data").get("user").get("profile").get("givenName")
+        familyName = req.get("originalRequest").get("data").get("user").get("profile").get("familyName")
+        return {
+            "speech": "Allowed",
+            "displayText": "Allowed",
+	    "followupEvent": {"name":"ProductSearch-FollowupEvent", "data":{"displayName":displayName, "givenName":givenName , "familyName":familyName}},
+            "data": {"displayName":displayName, "givenName":givenName , "familyName":familyName},
+            # "contextOut": [],
+            "source": "python-webhook"
+        }
+    elif (channel == "desktop" and req.get("result").get("action") == "request_name_permission"):
+        return {
+            "speech": "Allowed",
+            "displayText": "Allowed",
+	    "followupEvent": {"name":"ProductSearch-FollowupEvent", "data":{"displayName":userIdHybris, "givenName":userIdHybris , "familyName":userIdHybris}},
+            "data": {"displayName":userIdHybris, "givenName":userIdHybris , "familyName":userIdHybris},
+            # "contextOut": [],
+            "source": "python-webhook"
+        }
+    elif req.get("result").get("action") == "request_name_permission" or req.get("result").get("action") == "read_mind" or req.get("result").get("action") == "actions_intent_PERMISSION" :
+        baseurl = "https://us-central1-highstreet-bot-v4-1.cloudfunctions.net/getNameFunction"
+    else:
+        baseurl = "http://34.203.152.187/highstreetcommercewebservices/v2/highstreet/webhook/"
+    
+    
+    reqObj = urllib.request.Request(baseurl)
+    reqObj.add_header('Content-Type', 'application/json; charset=utf-8')
+    jsondata = json.dumps(req)
+    jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
+    reqObj.add_header('Content-Length', len(jsondataasbytes))
+    result = urlopen(reqObj,jsondataasbytes).read()
+    #data = json.loads(result)
+    #res = makeWebhookResult(data,req)
+    return result
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
